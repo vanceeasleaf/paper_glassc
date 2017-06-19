892db059377d24223990517394ac90f5d6b97998
Yang Zhou @cluster <404422239@qq.com>
2017-06-19 22:42:52 +0800

    add src

---
+++ src/sample.1/sub.py
@@ -0,0 +1,17 @@
+from aces import Aces
+class sub(Aces):
+	def submit(self):
+		opt=dict(
+			units="metal",
+			species="glassC",
+			method="greenkubo",
+			nodes=2,
+			procs=12,
+			queue="q1.1",
+			runTime=10000000
+			,runner="shengbte"
+		)
+		app=dict(supercell=[2,2,2],kpoints=[5,5,5])
+		self.commit(opt,app);
+if __name__=='__main__':
+	sub().run()
